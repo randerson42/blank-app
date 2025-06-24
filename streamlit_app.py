49d86f47fffb8031cf1ca69504d1bab723037e6b
randerson42 <robert@shirecm.com>
2025-06-24 14:49:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ Make_url = "https://hook.us2.make.com/aseggcntpt5uilz5091p7rrjno87ooyi"
 # Columns to place buttons side by side
 st.markdown("<br>", unsafe_allow_html=True)
 phone = st.text_input("📱 Enter your phone number")
+query = st.text_input("Specific Query?")
 col1, col2 = st.columns(2)
 
 # Button logic
@@ -15,16 +16,13 @@ with col1:
     if st.button("Daily Report"):
         st.write(f"📅 You clicked Daily Summary for:")
         # Insert daily summary logic here
-        payload = {"message": "test", "source": "streamlit"}
+        payload = {"phone": phone, "query": query, "source": "streamlit"}
         response = requests.post(Make_url, json=payload)
 
 with col2:
     if st.button("Weekly Report"):
         st.write(f"🗓️ You clicked Weekly Summary for: '{user_input}'")
         # Insert weekly summary logic here
-
-# Text input
-user_input = st.text_input("Specific Query Report:")
 
 # Optional debug output
 st.markdown("<br>", unsafe_allow_html=True)
